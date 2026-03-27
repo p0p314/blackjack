8ddf3ea495bbdc980280934eba0cf099cb574268
Cédric Popille <86252008+p0p314@users.noreply.github.com>
2026-03-27 02:02:20 +0100

    Refactor ESLint configuration to use modules

--- blackjack/eslint.config.mjs
+++ blackjack/eslint.config.mjs
@@ -1,7 +1,23 @@
-languageOptions: {
-  ecmaVersion: "latest",
-  sourceType: "module",
-  globals: {
-    ...globals.browser
-  }
-}
+import js from "@eslint/js";
+import prettierPlugin from "eslint-plugin-prettier";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
+];
